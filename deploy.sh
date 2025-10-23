f9857d3a0806a46eff3e0f525d490471de45b0c9
dsomni <pro100pro10010@gmail.com>
2025-10-24 02:01:00 +0300

    Add [deploy script]

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo "Installing dependencies"
+yarn install > /dev/null 2> /dev/null
+echo "Start to build your package"
+yarn build > /dev/null 2> /dev/null
+
+echo "Deploying"
+rm -rf /var/www/html/portfolio
+mkdir -p /var/www/html/portfolio
+mv ./dist/* /var/www/html/portfolio/
+
+echo "Finished!"
